c968782a8a022824f603d15be06c2ce0fd7c6a80
HundredPercentCoverage <bunglenutter@gmail.com>
2022-02-03 23:33:09 +0000

    Change backrgound colour

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,10 +1,12 @@
-import React, { ReactNode } from 'react'
+import { ReactNode } from 'react'
 
 export function Layout({ children }: { children?: ReactNode }) {
   return (
     <>
-      <div className="w-full flex items-center justify-center py-4">
-        <h1 className="text-5xl font-bold tracking-tight"><em className="text-amber-500">Flash</em>Cards</h1>
+      <div className="w-full flex items-center justify-center py-4 bg-gray-700">
+        <h1 className="text-5xl font-bold tracking-tight text-white">
+          <em className="text-amber-500">Flash</em>Cards
+        </h1>
       </div>
       <main>
         {children}
